79c2bc5cb63bf91d126fa28fbb7cb6f75d640331
Amy Steel <amylouisesteel@hotmail.co.uk>
2022-07-05 18:38:46 +0100

    Solution 2 added.

---
+++ git-demo-2/solution2.js
@@ -0,0 +1,7 @@
+const teachingTeam = [ 'Leo', 'Kirti', 'PJ', 'Gemma', 'James', 'Hello', 'P' ];
+
+for (let i = 0; i < teachingTeam.length; i++) {
+    teachingTeam[i] = teachingTeam[i].toUpperCase();
+}
+​
+console.log(teachingTeam);
