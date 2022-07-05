6109e25c66d0e8b01e6d7bc937b835daf3789d24
Amy Steel <amylouisesteel@hotmail.co.uk>
2022-07-05 19:02:54 +0100

    Add Solution 3.1

---
+++ solution3.1.js
@@ -0,0 +1,12 @@
+let lengthOfLongestName = 0;
+​
+for (let member of teachingTeam) {
+    if (member.length > lengthOfLongestName)
+        lengthOfLongestName = member.length;
+}
+​
+// scan 2: display names of that longest length.
+for (let member of teachingTeam) {
+    if (member.length === lengthOfLongestName)
+        console.log(member);
+}
