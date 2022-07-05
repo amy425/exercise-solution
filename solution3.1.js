cd735a7fc668456b1c8774a433fa93695e103e10
Amy Steel <amylouisesteel@hotmail.co.uk>
2022-07-05 20:23:51 +0100

    Add index.html for hosting

--- solution3.1.js
+++ solution3.1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const teachingTeam = [ 'Leo', 'Kirti', 'PJ', 'Gemma', 'James', 'Hello', 'P' ];
 ​
 let result = [];
@@ -17,17 +16,4 @@ for (member of teachingTeam) {
     else {  // member.length < lengthOfLongestName
         // do nothing
     }
-=======
-let lengthOfLongestName = 0;
-​
-for (let member of teachingTeam) {
-    if (member.length > lengthOfLongestName)
-        lengthOfLongestName = member.length;
-}
-​
-// scan 2: display names of that longest length.
-for (let member of teachingTeam) {
-    if (member.length === lengthOfLongestName)
-        console.log(member);
->>>>>>> q3
 }
